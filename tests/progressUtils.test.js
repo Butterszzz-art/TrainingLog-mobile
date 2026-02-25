0abbef38afc7d97f6164a2b2b3dea746f58cec30
Butterszzz-art <armanbahalibusiness@gmail.com>
2026-02-25 15:22:25 +0100

    Add weekly monotony and strain analysis to Progress tab

--- tests/progressUtils.test.js
+++ tests/progressUtils.test.js
@@ -1,4 +1,9 @@
-const { computeOneRepMax, updatePRs } = require('../progressUtils');
+const {
+  computeOneRepMax,
+  updatePRs,
+  calculateMonotony,
+  calculateStrain
+} = require('../progressUtils');
 const { calculateWorkoutVolume } = require('../calculateWorkoutVolume');
 
 test('computeOneRepMax calculates epley estimate', () => {
@@ -12,4 +17,14 @@ test('updatePRs updates record when higher', () => {
   const workout = { log: [{ exercise:'Bench', weightsArray:[100], repsArray:[5] }] };
   const prs = updatePRs(user, workout, calculateWorkoutVolume);
   expect(prs.Bench.oneRM).toBeGreaterThan(0);
+});
+
+test('calculateMonotony returns mean divided by standard deviation', () => {
+  const dailyLoads = [100, 200, 100, 200, 100, 200, 100];
+  const monotony = calculateMonotony(dailyLoads);
+  expect(monotony).toBeCloseTo(2.8868, 3);
+});
+
+test('calculateStrain multiplies monotony and weekly load', () => {
+  expect(calculateStrain(2, 1500)).toBe(3000);
 });
