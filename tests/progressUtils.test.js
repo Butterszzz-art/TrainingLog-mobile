4928d130c21d5993523105217edeea0c857905a1
Butterszzz-art <armanbahalibusiness@gmail.com>
2026-02-25 15:17:48 +0100

    Add automated workout volume, intensity, and 1RM metrics

--- tests/progressUtils.test.js
+++ tests/progressUtils.test.js
@@ -1,4 +1,4 @@
-const { computeOneRepMax, updatePRs } = require('../progressUtils');
+const { computeOneRepMax, calculate1RM, calculateWorkoutMetrics, updatePRs } = require('../progressUtils');
 const { calculateWorkoutVolume } = require('../calculateWorkoutVolume');
 
 test('computeOneRepMax calculates epley estimate', () => {
@@ -12,4 +12,19 @@ test('updatePRs updates record when higher', () => {
   const workout = { log: [{ exercise:'Bench', weightsArray:[100], repsArray:[5] }] };
   const prs = updatePRs(user, workout, calculateWorkoutVolume);
   expect(prs.Bench.oneRM).toBeGreaterThan(0);
+});
+
+
+test('calculate1RM supports brzycki method', () => {
+  const orm = calculate1RM(5, 100, 'brzycki');
+  expect(orm).toBeCloseTo(100 * (36 / (37 - 5)));
+});
+
+test('calculateWorkoutMetrics returns volume, intensity, and estimated 1RM', () => {
+  const metrics = calculateWorkoutMetrics({
+    log: [{ repsArray: [5, 8], weightsArray: [100, 80] }]
+  });
+  expect(metrics.sessionVolume).toBe(1140);
+  expect(metrics.averageIntensity).toBeGreaterThan(0);
+  expect(metrics.estimated1RM).toBeGreaterThan(100);
 });
